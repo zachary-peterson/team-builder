3bf8a15e9732e41a2ac6ed1e1c846046ef9028e0
Zachary Peterson <zp.lambda@yahoo.com>
2020-07-21 18:47:05 -0400

    ...

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Form from './Form';
+import Member from './Member'
 import './App.css';
 
+const initialTeamRoster = [
+  {
+    name: 'Gabe',
+    email: 'gabriel@lambdaschool.com',
+    role: 'Project Lead'
+  },
+]
+
+const initialForm = {
+  name: '',
+  email: '',
+  role: ''
+}
+
 function App() {
+  const [teamMembers, setTeamMembers] = useState(initialTeamRoster);
+  const [formValues, setFormValues] = useState(initialForm);
+
+  const formUpdate = (inputName, inputValue) => {
+    setFormValues({ ...formValues, [inputName]: inputValue });
+  }
+
+  const formSubmit = () => {
+    const newTeamMember = {
+      name: formValues.name,
+      email: formValues.email,
+      role: formValues.role
+    }
+
+    if(!formValues.name || !formValues.email || formValues.role) return;
+
+    setTeamMembers(newTeamMember);
+  }
+
+  console.log(teamMembers)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Lambda Dev Team</h1>
       </header>
+        <div>
+          <Form 
+          values={formValues}
+          update={formUpdate}
+          submit={formSubmit} 
+          />
+        </div>
+        <div>
+          {
+            teamMembers.map(member => {
+              
+                return (
+                <Member key={teamMembers.length + 1} details={member}/>
+              )
+            }
+          )}
+        </div>
     </div>
-  );
+)
+
+
+
 }
 
 export default App;
